95cd71476fdaed32b50aaab9823bd26c573ff7f9
nima afsharfar <46896398+ne3mer@users.noreply.github.com>
2025-12-04 14:47:20 +0100

    feat: Integrate Tailwind CSS, upgrade Node.js to v22, and configure Vite server settings.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Listen on all addresses
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
 })
